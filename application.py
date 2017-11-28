76dd1fa61f201c3b5e8cf9e8733916b1abb18d7d
Rakkan Jintasatien <rakkan.j@ku.th>
2017-11-28 10:58:09 +0700

    complete

--- application.py
+++ application.py
@@ -1,11 +1,3 @@
-# from tkinter import *
-# from tkinter import ttk
-#
-# int a=0
-# root = Tk()
-# Label(root, text="Hello, Tkinter").pack()
-# root.mainloop()
-# !/usr/bin/env python
 from tkinter import *
 from tkinter.filedialog import *
 import tkinter as tk
@@ -27,21 +19,10 @@ def cbc(id, tex):
     if id==4:
         return lambda: save(id, tex)
 
-
-def callback(id, tex):
-    s = 'At {} f is {}\n'.format(id, id ** id / 0.987)
-    tex.insert(tk.END, s)
-    tex.see(tk.END)  # Scroll if necessary
-
 ICMP_ECHO_REQUEST = 8
 ICMP_CODE = socket.getprotobyname('icmp');
 
-"""
-Given the bytes array, it calculates the checksum and returns it.
-"""
 def checksum(source_string):
-    # I'm not too confident that this is right but testing seems to
-    # suggest that it gives the same answers as in_cksum in ping.c.
     sum = 0
     count_to = (len(source_string) / 2) * 2
     count = 0
@@ -57,30 +38,17 @@ def checksum(source_string):
     sum = sum + (sum >> 16)
     answer = ~sum
     answer = answer & 0xffff
-    # Swap bytes. Bugger me if I know why.
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-"""
-Create a new echo request packet based on the given "id".
-"""
 def create_packet(id):
-    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
     header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0, 0, id, 1)
     data = ''
-    # Calculate the checksum on the data and the dummy header.
     my_checksum = checksum(header + data.encode('utf-8'))
-    # Now that we have the right checksum, we put that in. It's just easier
-    # to make up a new header than to stuff it into the dummy.
     header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
             socket.htons(my_checksum), id, 1)
     return header + data.encode('utf-8')
 
-"""
-Receive the ping from the socket.
-Returns 0 if a timeout occurs.
-Returns (src_ip_address, ping_time_milliseconds) if successful.
-"""
 def receive_ping(my_socket, packet_id, time_sent, timeout):
     time_left = timeout
     while True:
@@ -91,36 +59,23 @@ def receive_ping(my_socket, packet_id, time_sent, timeout):
             return 0
         time_received = time.time()
         rec_packet, addr = my_socket.recvfrom(1024)
-        # The last 8 bytes are the header of the packet we sent to the server
         icmp_header = rec_packet[-8:]
         type, code, checksum, p_id, sequence = struct.unpack(
                 'bbHHh', icmp_header)
         if p_id == packet_id:
             total_time_ms = (time_received - time_sent) * 1000
-            # Round to 3 decimal places:
             total_time_ms = math.ceil(total_time_ms * 1000) / 1000
             return (addr[0], total_time_ms)
         time_left -= time_received - time_sent
         if time_left <= 0:
             return 0
 
-"""
-Sends an ICMP ping to the given host, and gets the response for that ping.
-It sets the TTL in the IP header of the packet to the given value.
-Returns 0 if a timeout occurs.
-Returns (src_ip_address, ping_time_milliseconds) if successful.
-"""
 def echo_one(host, ttl):
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
     my_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-
-    # Maximum for an unsigned short int c object counts to 65535 so
-    # we have to sure that our packet id is not greater than that.
     packet_id = int(random.random() * 65535)
     packet = create_packet(packet_id)
     while packet:
-        # The icmp protocol does not use a port, but the function
-        # below expects it, so we just give it a dummy port.
         sent = my_socket.sendto(packet, (host, 1))
         packet = packet[sent:]
 
@@ -128,13 +83,6 @@ def echo_one(host, ttl):
     my_socket.close()
     return ping_res
 
-"""
-Given the host and a TTL value, it sends 3 pings.
-Formats a nice user friendly string.
-Returns (user_friendly_string, destination_reached).
-destination_reached is True if the IP address who replied matches the host,
-and False otherwise.
-"""
 def echo_three(host, ttl):
     try1 = echo_one(host, ttl)
     try2 = echo_one(host, ttl)
@@ -163,26 +111,28 @@ def echo_three(host, ttl):
 
     return (final_string, destination_reached)
 
-# -------------------------- #
-# Main execution starts here #
-# -------------------------- #
-
 top = tk.Tk()
+top.title("Datacom")
+topFrame = Frame(top,height = 500,width=200)
+topFrame.pack()
+
+
+lblInfo = tk.Label(topFrame,font=('arial',30,'bold'),text = "Comparator").grid(row=0,pady=10)
+lblInfo = tk.Label(topFrame,font=('arial',15,'normal'),text = "This tool can compare between nslookup and traceroute").grid(row=1,pady=5)
+
+middleFrame = Frame(top,height = 500,width=200)
+middleFrame.pack()
+
 tex = tk.Text(master=top)
 textCompare = tk.Text(master=top)
-tex.pack(side=tk.RIGHT)
+tex.pack(side=tk.LEFT)
 textCompare.pack(side=tk.RIGHT)
 bop = tk.Frame()
 bop.pack(side=tk.LEFT)
 v = StringVar()
-scanner = Entry(bop, textvariable=v)
-scanner.pack(padx=5)
+scanner = Entry(middleFrame, textvariable=v , width=25)
+scanner.pack(side=tk.LEFT,padx=5)
 
-# if len(sys.argv) <= 1:
-#     print('Bad usage. Provide a hostname.')
-#     sys.exit(1)
-
-# Domain name to IP address conversion:
 timeout = 3
 max_tries = 30
 
@@ -194,8 +144,6 @@ def trace(id,tex):
          ' hops max.\n')
 
     try:
-        # Loop until we hit the maximum number of hops, or until we reach the
-        # final destination host:
         for x in range(1, max_tries + 1):
             (line, destination_reached) = echo_three(host, x)
             s += line + '\n'
@@ -241,29 +189,29 @@ def compare():
 
 def save(id,tex):
     f = asksaveasfile(mode='w', defaultextension=".txt")
-    if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
+    if f is None: 
         return
-    text2save = tex.get("1.0", END) # starts from `1.0`, not `0.0`
+    text2save = tex.get("1.0", END)
     f.write(text2save)
-    f.close() # `()` was missing.
+    f.close() 
 
 k=1
 
-b = tk.Button(bop, text="nslookup", command=cbc(k,tex))
-b.pack()
+b = tk.Button(middleFrame, text="Nslookup", command=cbc(k,tex))
+b.pack(side=tk.LEFT)
 
 k=2
 
-b = tk.Button(bop, text="traceroute", command=cbc(k,tex))
-b.pack()
+b = tk.Button(middleFrame, text="Traceroute", command=cbc(k,tex))
+b.pack(side=tk.LEFT)
 
 k = 4
 
-r = tk.Button(bop, text="saveData", command=cbc(k,tex))
-r.pack()
+r = tk.Button(middleFrame, text="SaveData", command=cbc(k,tex))
+r.pack(side=tk.LEFT)
 
-choose = tk.Button(bop, text="compare", command=compare)
-choose.pack()
+choose = tk.Button(middleFrame, text="Compare", command=compare)
+choose.pack(side=tk.LEFT)
 
-tk.Button(bop, text='Exit', command=top.destroy).pack()
+tk.Button(middleFrame, text='Exit', command=top.destroy).pack()
 top.mainloop()
